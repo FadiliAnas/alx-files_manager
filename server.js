b3481aef64c4fbea2a1ff8bc8ad7b231753fab37
FadiliAnas <anasfadili65@gmail.com>
2025-06-27 18:48:38 +0200

    wget

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {

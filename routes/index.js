854e52e2907956873e9e7f03c851755df792de80
FadiliAnas <anasfadili65@gmail.com>
2025-06-28 06:08:19 +0200

    ddddddddddddddddddd

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,29 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+import express from "express"
+import AppController from "../controllers/AppController.js"
+import UsersController from "../controllers/UsersController.js"
+import AuthController from "../controllers/AuthController.js"
+import FilesController from "../controllers/FilesController.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
+// App routes
+router.get("/status", AppController.getStatus)
+router.get("/stats", AppController.getStats)
 
-export default router;
+// User routes
+router.post("/users", UsersController.postNew)
+router.get("/users/me", UsersController.getMe)
+
+// Auth routes
+router.get("/connect", AuthController.getConnect)
+router.get("/disconnect", AuthController.getDisconnect)
+
+// File routes
+router.post("/files", FilesController.postUpload)
+router.get("/files/:id", FilesController.getShow)
+router.get("/files", FilesController.getIndex)
+router.put("/files/:id/publish", FilesController.putPublish)
+router.put("/files/:id/unpublish", FilesController.putUnpublish)
+router.get("/files/:id/data", FilesController.getFile)
+
+export default router
